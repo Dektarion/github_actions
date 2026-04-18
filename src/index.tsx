c2e728479d43af245314dede91a3d4e8a014f2a5
Andrey Boyarsky <andreiy.boyarsky@gmail.com>
2026-04-18 19:07:44 +0300

    add: path2

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './app';
 import { Analytic } from './context/analityc';
-// import './styles.css';
+import './styles.css';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
